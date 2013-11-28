46cba01d0ca82d849fd574e7bbd66d911a22fb98
unknown <bajracharya.surendra@gmail.com>
2013-11-28 14:50:36 -0600

    fixed typo and added INSTALLED_APPS and related_name in models

--- kharcha_byawasthapan/hisabkitab/models.py
+++ kharcha_byawasthapan/hisabkitab/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class ExpenseItem(models.Model):
-    name=models.Charfield(max_length=100)
+    name=models.CharField(max_length=100)
 
 class Expense(models.Model):
     expenseItem = models.ForeignKey(ExpenseItem)
     expense_date=models.DateTimeField('expense date')
     amount=models.DecimalField(max_digits=6,decimal_places=2)
-    comments=models.TextTfield()
+    comments=models.TextField()
     user=models.ForeignKey(User)
-    borrower=models.ForeignKey(User)
+    borrower=models.ForeignKey(User,related_name="borrower")
