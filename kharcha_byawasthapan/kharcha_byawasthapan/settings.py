9951aad4ddd15e084c5981a09d51b3bd95a31f06
Ganga Bohara <gangabohara@Gangas-MacBook-Pro.local>
2013-11-28 14:32:20 -0600

    typo

--- kharcha_byawasthapan/kharcha_byawasthapan/settings.py
+++ kharcha_byawasthapan/kharcha_byawasthapan/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'kharchar_byawasthapan',                      # Or path to database file if using sqlite3.
+        'NAME': 'kharcha_byawasthapan',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
