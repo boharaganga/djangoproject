c9f6be7cd09b57697e0ed2c6203cab44bf1b79c5
unknown <bajracharya.surendra@gmail.com>
2013-11-28 14:29:15 -0600

    Created models and updated sql info

--- kharcha_byawasthapan/hisabkitab/models.py
+++ kharcha_byawasthapan/hisabkitab/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ExpenseItem(models.Model):
+    name=models.Charfield(max_length=100)
+
+class Expense(models.Model):
+    expenseItem = models.ForeignKey(ExpenseItem)
+    expense_date=models.DateTimeField('expense date')
+    amount=models.DecimalField(max_digits=6,decimal_places=2)
+    comments=models.TextTfield()
+    user=models.ForeignKey(User)
+    borrower=models.ForeignKey(User)
--- kharcha_byawasthapan/kharcha_byawasthapan/settings.py
+++ kharcha_byawasthapan/kharcha_byawasthapan/settings.py
@@ -11,13 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'kharchar_byawasthapan',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -121,7 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
